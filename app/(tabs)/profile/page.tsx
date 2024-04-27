a1e6bb32cf3b8e3b83c3febf03e47cc8d96658f9
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-04-28 07:42:12 +0900

    my_page_1

--- app/(tabs)/profile/page.tsx
+++ app/(tabs)/profile/page.tsx
@@ -1,43 +1,73 @@
+import ListProduct from "@/components/list-product";
 import db from "@/lib/db";
 import getSession from "@/lib/session";
 import { notFound, redirect } from "next/navigation";
 import { Suspense } from "react";
 
-async function getUser() {
-  const session = await getSession();
-  if (session.id) {
-    const user = await db.user.findUnique({
-      where: {
-        id: session.id,
-      },
-    });
-    if (user) {
-      return user;
-    }
-  }
-  notFound();
+interface UsernameProps {
+  username: string;
 }
 
-async function Username() {
-  const user = await getUser();
-  return <h1>Welcome! {user?.username}!</h1>;
+function Username({ username }: UsernameProps) {
+  return <h1>Welcome! {username}!</h1>;
+}
+
+async function getAllProducts(id: number) {
+  const products = await db.product.findMany({
+    orderBy: {
+      created_at: "desc",
+    },
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      photo: true,
+      created_at: true,
+      user: {
+        select: {
+          id: true,
+          username: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  return products.filter((product) => product.user?.id === id);
 }
 
 export default async function Profile() {
-  const logOut = async () => {
+  const session = await getSession();
+  const myprofile = await getAllProducts(session.id!);
+  const user = await db.user.findUnique({
+    where: {
+      id: session.id,
+    },
+    select: {
+      email: true,
+      username: true,
+      avatar: true,
+    },
+  });
+
+  async function logOut() {
     "use server";
     const session = await getSession();
-    await session.destroy();
+    session.destroy();
     redirect("/");
-  };
+  }
   return (
     <div>
       <Suspense fallback={"Welcome!"}>
-        <Username />
+        <Username username={user?.username ?? ""} />
       </Suspense>
-      <form action={logOut}>
+      <form className="flex right-0" action={logOut}>
         <button>Log out</button>
       </form>
+      <div className="w-full h-[60%] overflow-y-scroll   pb-10">
+        {myprofile.map((product) => (
+          <ListProduct isLikedByUser={false} key={product.id} {...product} />
+        ))}
+      </div>
     </div>
   );
 }
